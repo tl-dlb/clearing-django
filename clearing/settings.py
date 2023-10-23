47c89fc33d79db3f2306c0eda558aa7c6a7e4ed9
idxspecial <idxspecial@gmail.com>
2023-10-23 11:34:09 +0600

    hosts

--- clearing/settings.py
+++ clearing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%9ufu92)(x9y7zxd$epudk#w41cj-&u@g=9-97)jc0l2rsymhz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['host.docker.internal', 'localhost', 'cl.tsst.kz']
 
 
 # Application definition
